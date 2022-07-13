83fa006c3d799a723d7c05b9a8a98d670811006f
Rustam <rustam@Mac-mini-user.local>
2022-07-13 18:23:44 +0400

    header

---
+++ src/components/Filter.vue
@@ -0,0 +1,10 @@
+<script></script>
+
+<template>
+  <div class="filter">
+    <div class="filter__price">Price pange</div>
+    <div class="filter__rating"></div>
+  </div>
+</template>
+
+<style></style>
